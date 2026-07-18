531c0340eae2b15ec461ba897160ddf419d5ab0f
Ishit-Parikh <ishitparikh25@gmail.com>
2026-07-18 11:37:40 +0530

    Now starting the 12 Challenge of the course

---
+++ section-05-OOP/CodeExe12.cs
@@ -0,0 +1,20 @@
+using Section05;
+
+/*
+Create a simple C# program that:
+Declares a class Person with the following:
+A private member variable _name of type string.
+A public property Name with a getter and setter to access _name.
+A private member variable _age of type int.
+A public property Age with a getter and a custom setter that only sets _age if the value is greater than 0.
+
+Adds a constructor to Person that takes two parameters, name and age, and initializes the member variables.
+Adds a method Greet that prints a greeting message including the person's name and age.
+
+Demonstrates the use of the class in the Main method by creating an instance of Person and calling the Greet method.
+
+Alert!
+The result of execution for the default string should be:
+"Hello, my name is John and I am 30 years old."
+
+*/
